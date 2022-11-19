fdf79e6e0dc5b0711dea904cf2d9ec6fd02057b6
Renggeng Zheng <rzhengcontacts@gmail.com>
2022-11-19 16:01:27 -0500

    subplot labelling is now going better

--- graphing.py
+++ graphing.py
@@ -4,6 +4,7 @@ numeric = Union[float, int]
 
 import matplotlib.pyplot as plt
 import numpy as np
+from math import *
 
 #np.random.seed(42)
 
@@ -70,7 +71,7 @@ def generate_random_data(data_points: int) -> tuple:
 
 # Data on the effect of each process (?) on the measured variables.
 generated_data: tuple
-generated_data = generate_random_data(5)
+generated_data = generate_random_data(25)
 
 def graph_category(category: str, data: tuple = generated_data) -> None:
     """
@@ -79,14 +80,19 @@ def graph_category(category: str, data: tuple = generated_data) -> None:
     category:
         the category you want graphed
     """
+    # calculates the number of mappings and determine number of plots needed
+    number_of_mappings = len(data)
+    mappings_per_side = ceil(number_of_mappings ** (1/2))
+    # creates the figure and the subplots
+    fig, subplots = plt.subplots(ceil(number_of_mappings / mappings_per_side), mappings_per_side)
+
     for i in range(len(data)):
+        subplot = subplots[i // mappings_per_side, i % mappings_per_side]
         mapping_data = data[i]
         # Calculates the amount of bars you need. Assumes all components have even data for all categories.
         category_size = len(mapping_data[tuple(mapping_data.keys())[0]][category]) # accesses first component in memory, then checks the number of elements describing that category in component.
         index = np.arange(category_size) # a list representing all the elements in the category.
 
-        # defines the figure we're showing 
-        fig = plt.subplots(figsize = (10, 7))
         # the dictionary storing the bar contribution for each component
         plots = dict()
         # the bottom of where the bar contribution should be, so that it starts when the last contribution stops
@@ -96,18 +102,19 @@ def graph_category(category: str, data: tuple = generated_data) -> None:
         components: str
         metric_data: dict
         for (component, metric_data) in mapping_data.items():
-            print(tuple(metric_data[category].values()))
             #Creates the plots for each component
-            plots[component] = plt.bar(index, tuple(metric_data[category].values()), width, bottom = bottom_val)
+            plots[component] = subplot.bar(index, tuple(metric_data[category].values()), width, bottom = bottom_val)
             bottom_val += np.array(tuple(metric_data[category].values()))
         
         # Show title tick marks
-        plt.title("Relation of Structure to " + category)
-        plt.ylabel("Graph of Each Step")
-        plt.xticks(index, tuple(mapping_data[tuple(mapping_data.keys())[0]][category].keys()))
-        plt.yticks(np.arange(0, 300, 10))
-        plt.legend(tuple([plot[0] for plot in plots.values()]), tuple(mapping_data.keys()))
+        subplot.set_title(f"Mapping {i + 1}")
+        subplot.set_ylabel(category)
+        subplot.set_xticks(index, tuple(mapping_data[tuple(mapping_data.keys())[0]][category].keys()))
+        subplot.set_yticks(np.arange(0, 300, 10))
+        subplot.legend(tuple([plot[0] for plot in plots.values()]), tuple(mapping_data.keys()))
 
-        plt.show()
+    fig.suptitle("Relation of Structure to " + category)
 
 graph_category("Capacity")
+graph_category("Access Count")
+plt.show()

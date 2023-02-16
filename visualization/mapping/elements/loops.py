ec786b3a1b78023e4f20ec6ad43ed0c12d290e38
Reng Zheng <rzhengcontacts@gmail.com>
2023-02-16 04:34:57 -0500

    finished inits for the loops

--- visualization/mapping/elements/loops.py
+++ visualization/mapping/elements/loops.py
@@ -1,5 +1,5 @@
 class Loop(MappingElement):
-    """A mapping element representing a serial loop.
+    """A mapping element representing a generic loop.
 
     Attributes:
         dim: The dimension we're iterating along
@@ -12,3 +12,21 @@ class Loop(MappingElement):
         self.dim:str = dim
         self.start:int = start
         self.end:int = end
+
+class For(Loop):
+    """A mapping element representing a serial loop.
+    """
+
+    def __init__(self, dim:str, start:int, end:int):
+        """Inits the serial loop.
+        """
+        super(dim, start, end)
+
+class ParFor(Loop):
+    """A mapping element representing a parallel loop.
+    """
+
+    def __init__(self, dim:str, start:int, end:int):
+        """Inits the parallel loop.
+        """
+        super(dim, start, end)

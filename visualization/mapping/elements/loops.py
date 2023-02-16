b2e4f65e3b4fc0d9bf02eaac29b4c54fe98ee8b5
Reng Zheng <rzhengcontacts@gmail.com>
2023-02-16 10:45:24 -0500

    cleaned up loops

--- visualization/mapping/elements/loops.py
+++ visualization/mapping/elements/loops.py
@@ -1,3 +1,10 @@
+"""This module defines all loop types for a mapping.
+
+Typical usage example:
+ForLoop = For('X', 0, 4)
+"""
+
+
 class Loop(MappingElement):
     """A mapping element representing a generic loop.
 
@@ -7,26 +14,58 @@ class Loop(MappingElement):
         end: End of iteration.
     """
 
-    def __init__(self, dim:str, start:int, end:int):
+    def __init__(self, dim: str, start: int, end: int):
         """Inits Loop with dimension, start, and end"""
-        self.dim:str = dim
-        self.start:int = start
-        self.end:int = end
+        assert start < end, f"start:{start} >= end:{end}"
+        self._dim: str = dim
+        self._start: int = start
+        self._end: int = end
+
+    #####################
+    # DIM ACCESSOR FXNS #
+    #####################
+
+    @property
+    def dim(self) -> str:
+        """Getter fxn for self._dim"""
+        return self._dim
+
+    #######################
+    # START ACCESSOR FXNS #
+    #######################
+
+    @property
+    def start(self) -> str:
+        """Getter fxn for self._start"""
+        return self._start
+
+    #####################
+    # END ACCESSOR FXNS #
+    #####################
+
+    @property
+    def end(self) -> str:
+        """Getter fxn for self._end"""
+        return self._end
+
 
 class For(Loop):
-    """A mapping element representing a serial loop.
-    """
+    """A mapping element representing a serial loop."""
 
-    def __init__(self, dim:str, start:int, end:int):
-        """Inits the serial loop.
-        """
-        super(dim, start, end)
+    def __init__(self, dim: str, start: int, end: int):
+        """Inits the serial loop."""
+        super().__init__(dim, start, end)
+
+    def __str__(self):
+        return f"for {self._dim} in [{self.start}, {self.end})"
+
 
 class ParFor(Loop):
-    """A mapping element representing a parallel loop.
-    """
+    """A mapping element representing a parallel loop."""
 
-    def __init__(self, dim:str, start:int, end:int):
-        """Inits the parallel loop.
-        """
-        super(dim, start, end)
+    def __init__(self, dim: str, start: int, end: int):
+        """Inits the parallel loop."""
+        super().__init__(dim, start, end)
+
+    def __str__(self):
+        return f"par-for {self.dim} in [{self.start}, {self.end})"
